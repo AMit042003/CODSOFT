6ee8d99084a74498a290cf508e80d7e16bf8dd05
AMit042003 <89003352+AMit042003@users.noreply.github.com>
2024-05-22 11:34:49 +0530

    Update and rename GradeCalculator.java to GradeDeterminer.java

--- GradeDeterminer.java
+++ GradeDeterminer.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
 
-public class GradeCalculator {
+public class GradeDeterminer {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the number of subjects: ");
-        int numSubjects = scanner.nextInt();
+        int numSubjects = sc.nextInt();
 
         int totalMarks = 0;
 
         for (int i = 1; i <= numSubjects; i++) {
             System.out.print("Enter marks obtained in subject " + i + ": ");
-            int marks = scanner.nextInt();
+            int marks = sc.nextInt();
             totalMarks += marks;
         }
 
-        double averagePercentage = (double) totalMarks / (numSubjects * 100) * 100;
+        double avgPercent = (double) totalMarks / (numSubjects * 100) * 100;
 
         char grade;
-        if (averagePercentage >= 90) {
+        if (avgPercent >= 90) {
             grade = 'A';
-        } else if (averagePercentage >= 80) {
+        } else if (avgPercent >= 80) {
             grade = 'B';
-        } else if (averagePercentage >= 70) {
+        } else if (avgPercent >= 70) {
             grade = 'C';
-        } else if (averagePercentage >= 60) {
+        } else if (avgPercent >= 60) {
             grade = 'D';
         } else {
             grade = 'F';
         }
 
-        System.out.println("----------------------------");
+        System.out.println("*************************************************");
         System.out.println("Total Marks: " + totalMarks);
-        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Average Percentage: " + avgPercent + "%");
         System.out.println("Grade: " + grade);
 
-        scanner.close();
+        sc.close();
     }
 }
